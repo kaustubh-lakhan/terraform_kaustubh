ccc0ef1d75dc7f3946965fe50f0af0212c377a49
kaustubh-lakhan <kaustubhlakhan@gmail>
2025-02-08 01:12:04 +0530

    fixed fileafter practical

--- main.tf
+++ main.tf
@@ -5,8 +5,7 @@ provider "aws" {
 resource "aws_instance" "terraformPractical" {
   ami           = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.micro"
-  name = "terraform1"
   tags = {
-    Name = "HelloWorld"
+    Name = "instanceName1"
   }
 }
