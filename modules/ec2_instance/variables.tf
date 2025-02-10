b48ad00623364cd94ab7a97bd0806de34b02515d
kaustubh lakhan <kaustubhlakhan@gmail.com>
2025-02-11 01:01:39 +0530

    terraform update

--- modules/ec2_instance/variables.tf
+++ modules/ec2_instance/variables.tf
@@ -1,12 +1,12 @@
 
-variable "instance_type{
+variable "instance_type" {
     description="this is regarding instance type resource capacity"
     type=string
     default="t2.micro"
 }
 
-variable "ami_value"{
-    description="this is regarding instance type resource capacity"
+variable "ami_value" {
+    description= "this is regarding instance "
     type=string
     default="ami-04b4f1a9cf54c11d0"
 }
