b6d3b1147fcf9b1aedf0e07b1514badb2aecbf4b
Kaustubh Lakhan <162490638+kaustubh-lakhan@users.noreply.github.com>
2025-02-12 09:48:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,25 @@
 provider "aws"{
-    region="us-east-1"
+    region = "us-east-1"
+}
+
+variable "ami_value"{
+    description="this is regarding amazon machine image ID"
+    type=string
+}
+
+variable "instance_type"{
+    description="this is regarding amazon machine type"
+    type=string
+}
+
+variable "instance_name"{
+    description="this is regarding amazon name"
+    type=string
 }
 
 module "ec2_instance"{
-    source= "./modules/ec2_instance"    
-    ami_value=var.ami_value   
-    instance_type=var.instance_type
+    source = "./modules/ec2_instance"
+    ami_value = var.ami_value
+    instance_type = var.instance_type
+    instance_name = var.instance_name
 }
