33a88801770c4670268c905dc43ff145730767e1
Kaustubh Lakhan <162490638+kaustubh-lakhan@users.noreply.github.com>
2025-02-12 09:49:21 +0530

    Update variables.tf

--- modules/ec2_instance/variables.tf
+++ modules/ec2_instance/variables.tf
@@ -1,14 +1,15 @@
-
-variable "instance_type" {
-    description="this is regarding instance type resource capacity"
+variable "ami_value"{
+    description="this is regarding amazon machine image ID"
     type=string
-    default="t2.micro"
 }
 
-variable "ami_value" {
-    description= "this is regarding instance "
+variable "instance_type"{
+    description="this is regarding amazon machine type"
     type=string
-    default="ami-04b4f1a9cf54c11d0"
 }
 
+variable "instance_name"{
+    description="this is regarding amazon name"
+    type=string
+}
 
