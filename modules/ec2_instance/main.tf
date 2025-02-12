77ff929b1ecedda1c887624e77de8a49c9abe037
Kaustubh Lakhan <162490638+kaustubh-lakhan@users.noreply.github.com>
2025-02-12 09:49:44 +0530

    Update main.tf

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -1,8 +1,11 @@
-provider "aws" {
+provider "aws"{
     region ="us-east-1"
 }
 
-resource "aws_instance" "example"{
-    ami= var.ami_value
-    instance_type= var.instance_type
+resource "aws_instance" "instance_server"{
+    ami=var.ami_value
+    instance_type = var.instance_type
+    tags ={
+        Name= var.instance_name
+    }
 }
